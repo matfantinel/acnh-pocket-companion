895acacb4b7f39fc677ce7fed68de7fec729bf9c
Matheus Fantinel <matheus@fantinel.dev>
2020-05-25 22:08:47 -0300

    Android setup

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CoolHeaderComponent } from './cool-header/cool-header.component';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { SlideDrawerComponent } from './slide-drawer/slide-drawer.component';
 
 @NgModule({
-    declarations: [CoolHeaderComponent],
+    declarations: [CoolHeaderComponent, SlideDrawerComponent],
     imports: [CommonModule, IonicModule.forRoot()],
-    exports: [CoolHeaderComponent]
+    exports: [CoolHeaderComponent, SlideDrawerComponent]
 })
 export class ComponentsModule {}

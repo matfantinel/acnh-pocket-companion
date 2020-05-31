2685b166b44b934c1b44705b049807fe1a9d063d
Matheus Fantinel <matheus@fantinel.dev>
2020-05-31 18:11:10 -0300

    Improving the Nookphone-like transition

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -33,12 +33,12 @@ export class HomePage implements OnInit {
 
   ionViewDidEnter() {
     if (this.platform.is('mobile')) {
-      StatusBar.setBackgroundColor({ color: '#f5f2e3' });
+      StatusBar.setBackgroundColor({ color: '#ECE0BA' });
     }
   }
 
-  goToPage(event: any, color: string, route: string) {
-    let transitionEffectPixel = document.getElementById('transition-effect');
+  goToPage(event: any, color: string, route: string, icon: string, label: string) {
+    let transitionEffectPixel = document.getElementById('transition-effect');    
 
     transitionEffectPixel.style.height = '1px';
     transitionEffectPixel.style.width = '1px';
@@ -49,6 +49,20 @@ export class HomePage implements OnInit {
     transitionEffectPixel.style.display = 'block';
     transitionEffectPixel.style.backgroundColor = color;
 
+    let transitionIcon = (document.getElementById('transition-icon') as HTMLImageElement);
+    transitionIcon.src = `assets/icons/${icon}.png`;
+    transitionIcon.style.display = 'block';
+    transitionIcon.style.height = '1px';
+    transitionIcon.style.width = '1px';
+    transitionIcon.style.top = `${event.clientY}px`;
+    transitionIcon.style.left = `${event.clientX}px`;
+
+    let transitionLabel = document.getElementById('transition-label');
+
+    transitionLabel.textContent = label;
+    transitionLabel.style.display = 'block';
+    transitionLabel.style.opacity = '0';
+
     setTimeout(() => {
       transitionEffectPixel.style.transition = 'all .5s ease-in-out';
       transitionEffectPixel.style.top = '0px';
@@ -57,14 +71,33 @@ export class HomePage implements OnInit {
       transitionEffectPixel.style.width = '100vw';
       transitionEffectPixel.style.borderRadius = '0px';
 
+      transitionIcon.style.transition = 'all .5s ease-in-out';
+      transitionIcon.style.height = '90px';
+      transitionIcon.style.width = '90px';
+      transitionIcon.style.top = 'calc(50% - 45px)';
+      transitionIcon.style.left = 'calc(50% - 45px)';
+
+      transitionLabel.style.transition = 'all 1s ease-in-out';
+      transitionLabel.style.opacity = '1';
+
+      setTimeout(() => {
+        StatusBar.setBackgroundColor({ color: color });
+      }, 500)
+
       setTimeout(() => {
         this.router.navigate([route]);
 
         setTimeout(() => {
           transitionEffectPixel.style.display = 'none';
           transitionEffectPixel.style.transition = 'none';
+
+          transitionIcon.style.display = 'none';
+          transitionIcon.style.transition = 'none';
+
+          transitionLabel.style.display = 'none';
+          transitionLabel.style.transition = 'none';
         }, 300)
       }, 1500);
-    }, 10);
+    }, 100);
   }
 }

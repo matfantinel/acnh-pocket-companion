0b9c85efebec10e396971bb69c33ccd5a0433096
Matheus Fantinel <matheus@fantinel.dev>
2020-07-20 14:27:43 -0300

    Base layout functionality for Critterpedia

--- src/app/pages/critterpedia/critterpedia.page.ts
+++ src/app/pages/critterpedia/critterpedia.page.ts
@@ -13,15 +13,111 @@ const { StatusBar } = Plugins;
   styleUrls: ['./critterpedia.page.scss'],
 })
 export class CritterpediaPage implements OnInit {
+
+  activeSegment = 'bugs';
+
+  bugs: any[] = [];
+  fishes: any[] = [];
+  seaCreatures: any[] = [];
+  fossils: any[] = [];
+
   constructor(public utils: Utils, private platform: Platform, private store: Store<AppState>) {
   }
 
   ngOnInit() {
+    this.loadBugs();
+    this.loadFishes();
+    this.loadSeaCreatures();
+    this.loadFossils();
   }
 
   ionViewDidEnter() {
     if (this.platform.is('capacitor')) {
       StatusBar.setBackgroundColor({ color: '#B4D8E3' });
     }
+  }
+
+  segmentChanged(event: any) {
+    this.activeSegment = event.detail.value;
+  }
+
+  loadBugs(event?: any) {
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.bugs.push({
+          name: `Bug ${i + 1}`
+        });
+      }
+
+      if (event && event.target) {
+        // When loading is finished:
+        event.target.complete();
+      }
+
+      if (this.bugs.length >= 120) {
+        // When reached the end of data:
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  loadFishes(event?: any) {
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.fishes.push({
+          name: `Fish ${i + 1}`
+        });
+      }
+
+      if (event && event.target) {
+        // When loading is finished:
+        event.target.complete();
+      }
+
+      if (this.fishes.length >= 120) {
+        // When reached the end of data:
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  loadSeaCreatures(event?: any) {
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.seaCreatures.push({
+          name: `Sea Creature ${i + 1}`
+        });
+      }
+
+      if (event && event.target) {
+        // When loading is finished:
+        event.target.complete();
+      }
+
+      if (this.seaCreatures.length >= 120) {
+        // When reached the end of data:
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  loadFossils(event?: any) {
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.fossils.push({
+          name: `Fossil ${i + 1}`
+        });
+      }
+
+      if (event && event.target) {
+        // When loading is finished:
+        event.target.complete();
+      }
+
+      if (this.fossils.length >= 120) {
+        // When reached the end of data:
+        event.target.disabled = true;
+      }
+    }, 500);
   }
 }
